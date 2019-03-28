1c5c156300b35e151321c74dd525c158fa576f17
Minh Hai Le <minh.le@punch.local>
2019-03-29 00:18:58 +0700

    ve rua

---
+++ Session 2/hw/Turtle_1.py
@@ -0,0 +1,15 @@
+from turtle import *
+speed(5)
+pencolor("red")
+for i in range(4):
+    left(30)
+    forward(100)
+    right(60)
+    forward(100)
+    right(120)
+    forward(100)
+    right(60)
+    forward(100)
+    left(120)
+
+mainloop()  
---
+++ Session 2/hw/Turtle_2.py
@@ -0,0 +1,17 @@
+from turtle import *
+speed(5)
+pencolor("red")
+for i in range(6):
+    left(60)
+    forward(100)
+for i in range(4):
+    left(90)
+    forward(100)
+pencolor("blue")
+for i in range(5):
+    left(180-108)
+    forward(100)
+for i in range(3):
+    left(120)
+    forward(100)
+mainloop()  
