6dd8b810016857530ef67ab6edaa55506afd1856
Minh Hai Le <minh.le@punch.local>
2019-04-02 00:48:28 +0700

    yeah

--- Session3/hw/5_Turtle2.py
+++ Session3/hw/5_Turtle2.py
@@ -1,12 +1,17 @@
 from turtle import *
 speed(0)
-color("red","red")
-begin_fill()
-for i in range (2):
-    forward(50)
-    left(90)
-    forward(100)
-    left(90)
-end_fill()
-
-mainloop()
+mau = ['blue', 'yellow', 'red', 'brown', 'grey']
+stt = -1
+while stt <= 4:
+    for i in range (5):
+        stt +=1
+        color(mau[stt],mau[stt])
+        begin_fill()
+        for i in range (2):
+            forward(50)
+            left(90)
+            forward(100)
+            left(90)
+        forward(50)
+        end_fill()
+    mainloop()
