be092a3ed6e7dee6148596453cc7afc7cdbaefbb
Minh Hai Le <minh.le@punch.local>
2019-05-07 22:50:38 +0700

    bike

--- Web 1/hw 2/rent_bikie.py
+++ Web 1/hw 2/rent_bikie.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template,request
+from pymongo import MongoClient
+
 app = Flask(__name__)
 
-bike = []
+uri = 'mongodb+srv://admin:admin@c4e29-cluster-qtb0x.mongodb.net/test?retryWrites=true'
+
+client = MongoClient(uri)
+first_db = client.first_db
+bike_collection = first_db['bike_collection']
 
 @app.route('/new_bike',methods=["GET","POST"])
 def index():
@@ -15,7 +21,7 @@ def index():
             "Image_Link" : bike_form['Image'],
             "Year" : bike_form['Year'],
         }
-        bike.append(new_bike)
+        bike_collection.insert_one(new_bike)
         print(new_bike)
         return "Thank you for submitting"
 
